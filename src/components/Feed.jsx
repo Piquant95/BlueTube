88c83de88b66be08878c837ac7ab5323e9c17b09
Piquant95 <piquant95@yahoo.com>
2023-07-02 21:11:15 -0400

    set up video useState to import videos from APIs

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,9 +5,11 @@ import { Sidebar, Videos } from './';
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
   }, [selectedCategory]);
 
   return (
@@ -28,10 +30,10 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
         <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
-          New <span style={{ color: '#0015ff'}}>videos</span>
+          {selectedCategory} <span style={{ color: '#0015ff'}}>videos</span>
         </Typography>
 
-        <Videos videos={[]} />
+        <Videos videos={[videos]} />
       </Box>
     </Stack>
   )
