970de00889111bd1e50b1c60d42a5d86f37e7761
Piquant95 <piquant95@yahoo.com>
2023-07-04 15:39:31 -0400

    added typography for channel title

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -9,6 +9,9 @@ const ChannelCard = ({ channelDetail }) => {
         <Link to={`/channel/${channelDetail?.id?.channelId}`}>
             <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
                 <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}/>
+                <Typography variant="h6">
+                    {channelDetail?.snippet?.title}
+                </Typography>
             </CardContent>
         </Link>
     </Box>
