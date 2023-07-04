8a78a5e023476226e994fe07f76e39ef6cec0ece
Piquant95 <piquant95@yahoo.com>
2023-07-04 15:32:01 -0400

    configured ChannelCard.jsx to display thumbnail and channel detail

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
-  )
+import { demoProfilePicture } from '../utils/constants';
+
+const ChannelCard = ({ channelDetail }) => {
+    <Box sx={{ boxShadow: 'none', borderRadius: '20px',}}>
+        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+            <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
+                <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{ borderRadius: '50%', height: '180px', width: '180px' }}/>
+            </CardContent>
+        </Link>
+    </Box>
 }
 
 export default ChannelCard
