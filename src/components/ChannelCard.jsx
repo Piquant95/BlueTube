937661342acb14ecae0345cc8c076cf7cf51c240
Piquant95 <piquant95@yahoo.com>
2023-07-04 17:41:26 -0400

    fixed video rendering on channel page as well as channel thumbnail styling

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { demoProfilePicture } from '../utils/constants';
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
     <Box sx={{ 
         boxShadow: 'none', 
         borderRadius: '20px', 
@@ -13,7 +13,8 @@ const ChannelCard = ({ channelDetail }) => {
         alignItems: 'center', 
         width: { xs: '356px', md: '320px'},
         height: '326px',
-        margin: 'auto'}}>
+        margin: 'auto',
+        marginTop: marginTop,}}>
         <Link to={`/channel/${channelDetail?.id?.channelId}`}>
             <CardContent sx={{ 
                 display: 'flex', 
