a30487316dd924854402ea367af06a480ce4ef5d
Piquant95 <piquant95@yahoo.com>
2023-07-04 15:35:46 -0400

    adjusted channel card border radius to a more ideal length

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -8,7 +8,7 @@ const ChannelCard = ({ channelDetail }) => {
     <Box sx={{ boxShadow: 'none', borderRadius: '20px',}}>
         <Link to={`/channel/${channelDetail?.id?.channelId}`}>
             <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
-                <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{ borderRadius: '50%', height: '180px', width: '180px' }}/>
+                <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}/>
             </CardContent>
         </Link>
     </Box>
