3a8eebefd492240242bc8bd00e0528b58356f35c
Piquant95 <piquant95@yahoo.com>
2023-07-04 16:40:48 -0400

    fixed console undefined error.

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -17,7 +17,7 @@ const ChannelDetail = () => {
     fetchFromAPI(`channels?part=snippet&id=${id}`)
       .then((data) => setChannelDetail(data?.items[0]));
 
-    fetchFromAPI(`search?channelId=${id}&part=snippet&order=data`)
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
       .then((data) => setVideos(data?.items));
   }, [id])
 
