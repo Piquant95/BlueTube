db20b640b1a9d6dd8e7117bb97e729df0b7b66f8
Piquant95 <piquant95@yahoo.com>
2023-07-04 16:29:01 -0400

    imported useEffect into channelDetail and began implementing fetchFromAPI function

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './'; 
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI
+  }, [id])
   return (
     <div>ChannelDetail</div>
   )
