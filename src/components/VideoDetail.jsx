35ab6b27922845ba0ff0b2efd0a4029e16b447c7
Piquant95 <piquant95@yahoo.com>
2023-07-04 23:00:22 -0400

    fixed mapping issue

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -53,11 +53,10 @@ const VideoDetail = () => {
             </Stack>
           </Box>
         </Box>
+        <Box px={2} py= {{md: 1, xs: 5}} justifyContent="center" alignItems="center">
+          <Videos videos={videos} direction="column" />
+        </Box>
       </Stack>
-
-      <Box px={2} py= {{md: 1, xs: 5}} justifyContent="center" alignItems="center">
-        <Videos videos={videos}/>
-      </Box>
     </Box>
   )
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,10 +3,11 @@ import { Stack, Box } from "@mui/material";
 
 import { ChannelCard, VideoCard } from "./";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return 'Loading...';
  
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
