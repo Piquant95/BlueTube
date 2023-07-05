238065b40b9f8fb25495d6a01c64360d9a7113eb
Piquant95 <piquant95@yahoo.com>
2023-07-04 22:55:08 -0400

    configuring recommended videos on side of video detail page

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,16 +4,20 @@ import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
-import { Video } from './';
+import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-      .then((data) => setVideoDetail(data.items[0]))
+      .then((data) => setVideoDetail(data.items[0]));
+
+      fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items));
   }, [id]);
 
   if (!videoDetail?.snippet) return 'Loading...';
@@ -38,15 +42,22 @@ const VideoDetail = () => {
                     <CheckCircle  sx={{ fontSize: '12px', color: 'gray', ml: '5px'}} />
                   </Typography>
                 </Link>
-                <Stack>
+                <Stack direction="row" gap="20px" alignItems="center">
                   <Typography variant="body1" sx={{ opacity: 0.7 }}>
                     {parseInt(viewCount).toLocaleString()} views
+                  </Typography>
+                  <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                    {parseInt(likeCount).toLocaleString()} likes
                   </Typography>
                 </Stack>
             </Stack>
           </Box>
         </Box>
       </Stack>
+
+      <Box px={2} py= {{md: 1, xs: 5}} justifyContent="center" alignItems="center">
+        <Videos videos={videos}/>
+      </Box>
     </Box>
   )
 }
