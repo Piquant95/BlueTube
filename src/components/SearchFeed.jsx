bd0200087337c8b190d1f5d13ace63be58d1d3ad
Piquant95 <piquant95@yahoo.com>
2023-07-04 20:24:46 -0400

    Setting up layout for search feed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos } from './';
 
-const SearchFeed = () => {
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          {selectedCategory} <span style={{ color: '#0015ff'}}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
   )
 }
 
-export default SearchFeed
+export default Feed
