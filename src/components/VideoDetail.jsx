c40fdd3e635f271a81be0b8f5f8609f6b0d54f35
Piquant95 <piquant95@yahoo.com>
2023-07-04 21:03:25 -0400

    started creating layout for video detail page when a video is selected.

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { video } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box>
+            
+          </Box>
+        </Box>
+      </Stack>
+
+    </Box>
   )
 }
 
