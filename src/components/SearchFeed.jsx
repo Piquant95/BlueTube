504e0294a91f4e94421cd931f9a5f95d7cdf3792
Piquant95 <piquant95@yahoo.com>
2023-07-04 20:33:17 -0400

    imported useParams and implemented it so that each search term refreshes and shows a unique set of videos

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,26 +1,27 @@
 import { useState, useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Videos } from './';
 
-const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState('New');
+const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
       .then((data) => setVideos(data.items))
-  }, [selectedCategory]);
+  }, [searchTerm]);
 
   return (
     <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
-        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
-          {selectedCategory} <span style={{ color: '#0015ff'}}>videos</span>
-        </Typography>
+      <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+        Search Results for: <span style={{ color: '#0015ff'}}>videos</span>
+      </Typography>
 
-        <Videos videos={videos} />
-      </Box>
+      <Videos videos={videos} />
+    </Box>
   )
 }
 
-export default Feed
+export default SearchFeed
