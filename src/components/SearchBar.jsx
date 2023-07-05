8553d4d1422557ac1bbbf5802e4ddb3fdab58654
Piquant95 <piquant95@yahoo.com>
2023-07-04 20:43:12 -0400

    altered handleSubmit to allow request in searchbar to naviagte user to a new url for search term.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(searchTerm) {
+            Navigate(`/search/${searchTerm}`);
+
+            setSearchTerm('')
+        }
+    }
   return (
     <Paper
         component="form"
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         sx={{
             borderRadius: 20,
             border: '1px solid #e3e3e3',
