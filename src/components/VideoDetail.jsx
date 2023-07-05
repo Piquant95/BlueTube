1a1a1a3e95d0af5b64c216315898631624631038
Piquant95 <piquant95@yahoo.com>
2023-07-04 21:26:13 -0400

    video now plays and also added controls to video

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -8,10 +8,12 @@ import { video } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
+  const [VideoDetail, setVideoDetail] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]));
   }, [id]);
 
   return (
@@ -19,7 +21,8 @@ const VideoDetail = () => {
       <Stack direction={{ xs: 'column', md: 'row' }}>
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
-            <ReactPlayer />
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
+            className="react-player" controls />
           </Box>
         </Box>
       </Stack>
